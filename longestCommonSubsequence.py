e39939de430d7d75dc955b084f82449a8908c206
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-11-11 20:55:38 -0600

    Add main block for testing longestCommonSubsequence
    Added a main block to test the longestCommonSubsequence function.

--- longestCommonSubsequence.py
+++ longestCommonSubsequence.py
@@ -13,3 +13,10 @@ class Solution:
                 else:
                     dp[i][j] = max(dp[i][j + 1], dp[i + 1][j])
         return dp[0][0]
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    text1 = "abcde"
+    text2 = "ace"
+    print("Output is : ", sol.longestCommonSubsequence(text1,text2))
