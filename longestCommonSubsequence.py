dc5ac6fe1f0267e2ffe02aedce5c3639509ea460
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-11-11 20:54:42 -0600

    Initialize DP table for LCS calculation

--- longestCommonSubsequence.py
+++ longestCommonSubsequence.py
@@ -2,3 +2,9 @@
 #1143. Longest Common Subsequence
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+
+        
+        dp = [[0 for j in range(len(text2) + 1)] for i in range(len(text1) + 1)]
+
+        for i in range(len(text1) - 1, -1 , -1):
+            for j in range(len(text2) - 1, -1, -1):
